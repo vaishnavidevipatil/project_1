b500cd8bf13a4aa37cbb249e058a23d70b84033d
vaishnavidevipatil <145427408+vaishnavidevipatil@users.noreply.github.com>
2024-10-12 01:56:48 +0530

    Run file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from routes import items, clock_in
@@ -12,3 +13,8 @@ app.include_router(clock_in.router)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI MongoDB Application!"}
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
